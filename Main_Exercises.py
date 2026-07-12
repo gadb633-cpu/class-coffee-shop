375f57b63c9361873d697dab6f915f197082d368
gad-biton <gadb633@gmail.com>
2026-07-12 11:37:04 +0300

    added exercise 1

---
+++ Main_Exercises.py
@@ -0,0 +1,11 @@
+# 1. Describe a Menu Item
+class MenuItem:
+    def __init__(self,name,price):
+        self.name = name
+        self.price = price
+    def describe(self):
+        print( f"Item: {self.name} | Price: ${self.price}")
+
+Espresso = MenuItem("Espresso",3.5)
+Espresso.describe()
+        
